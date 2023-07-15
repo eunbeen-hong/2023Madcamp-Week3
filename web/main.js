3bda154d48a6fd62805d7eecbd0282395c7f42d4
pancakesontuesday <mulberry3280@korea.ac.kr>
2023-07-15 23:26:24 +0900

    web server

---
+++ web/main.js
@@ -0,0 +1,16 @@
+var http = require("http");
+var fs = require("fs");
+
+var app = http.createServer(function (req, res) {
+    var url = req.url;
+    if (req.url == "/") {
+        url = "/index2.html";
+    }
+    if (req.url == "/favicon.ico") {
+        return res.writeHead(404);
+    }
+    res.writeHead(200);
+    res.end(fs.readFileSync(__dirname + url));
+});
+
+app.listen(3000);
