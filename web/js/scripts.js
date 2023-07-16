e877dbdf100ad4e55ce7a62e1a6f1122731c0411
pancakesontuesday <mulberry3280@korea.ac.kr>
2023-07-16 15:06:31 +0900

    .

---
+++ web/js/scripts.js
@@ -0,0 +1,50 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector("#navbar");
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove("navbar-shrink");
+        } else {
+            navbarCollapsible.classList.add("navbar-shrink");
+        }
+    };
+
+    navbarShrink();
+
+    document.addEventListener("scroll", navbarShrink);
+
+    const navbar = document.body.querySelector("#navbar");
+    if (navbar) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: "#navbar",
+            rootMargin: "0px 0px -40%",
+        });
+    }
+
+    const navbarToggler = document.body.querySelector(".navbar-menu");
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll("#navbarResponsive .navbar-link")
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener("click", () => {
+            if (window.getComputedStyle(navbarToggler).display !== "none") {
+                navbarToggler.click();
+            }
+        });
+    });
+
+    function togglePopup() {
+        var popup = document.getElementById("game-popup1");
+        popup.classList.toggle("show");
+    }
+
+    var gameLinks = document.querySelectorAll(".game-link");
+    gameLinks.forEach(function (gameLink) {
+        gameLink.addEventListener("click", function (event) {
+            event.preventDefault();
+            togglePopup();
+        });
+    });
+});
